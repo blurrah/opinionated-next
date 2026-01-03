eee35dce6d15fe0388473a1214cd7dbefbade7b4
Boris Besemer <borisbesemer@gmail.com>
2026-01-03 14:41:02 +0100

    more more wippers

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["app/**/*.test.{ts,tsx}", "src/**/*.test.{ts,tsx}"],
+    include: ["./app/**/*.test.{ts,tsx}", "./src/**/*.test.{ts,tsx}"],
     exclude: [
       "**/node_modules/**",
-      "**/dist/**",
-      "**/.{idea,git,cache,output,temp}/**",
-      "**/tmp/**",
       "**/.evals-cache/**",
+      "**/tmp/**",
       "**/.next/**",
     ],
+    root: ".",
   },
 });
